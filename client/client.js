d9bf4edefb894950c9af574ec31fb3a9db273b12
Yanis Urbis <yanis.urbis@gmail.com>
2016-04-11 13:07:10 +0300

    Drop-down list

--- client/client.js
+++ client/client.js
@@ -1,22 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.selectHouse.helpers({
+  housesNameId: function () {
+    return HousesCollection.find({}, {})
   },
-});
+  isSelected: function () {
+    return Session.equals('selectedHouseId', this._id) ? 'selected' : ''
+  }
+})
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.selectHouse.events = {
+  'change #selectHouse': function (evt) {
+    Session.set('selectedHouseId', evt.currentTarget.value)
+  }
+}
