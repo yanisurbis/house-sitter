0d277ec63314a6d8aec7f8866bdcafc13c866067
Yanis Urbis <yanis.urbis@gmail.com>
2016-04-11 15:22:07 +0300

    House template

--- client/client.js
+++ client/client.js
@@ -1,6 +1,13 @@
+// selectHouse Template
+
+Tracker.autorun(() => {
+  console.log("The selectedHouse ID is :"
+     + Session.get('selectedHouseId'));
+})
+
 Template.selectHouse.helpers({
   housesNameId: function () {
-    return HousesCollection.find({}, {})
+    return HousesCollection.find({}, {fields: {name: 1, _id: 1}})
   },
   isSelected: function () {
     return Session.equals('selectedHouseId', this._id) ? 'selected' : ''
@@ -9,6 +16,14 @@ Template.selectHouse.helpers({
 
 Template.selectHouse.events = {
   'change #selectHouse': function (evt) {
+    //console.log(HousesCollection.findOne({_id: evt.currentTarget.value}))
     Session.set('selectedHouseId', evt.currentTarget.value)
   }
 }
+
+
+Template.showHouse.helpers({
+  house: () => {
+    return HousesCollection.findOne({_id: Session.get('selectedHouseId')})
+  }
+})
