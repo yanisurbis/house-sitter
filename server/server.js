f88d3a9cfee55674971ed5332fb83a83a7c87163
Yanis Urbis <yanis.urbis@gmail.com>
2016-04-11 12:48:15 +0300

    Setting up

---
+++ server/server.js
@@ -0,0 +1,22 @@
+Meteor.startup(() => {
+  if (HousesCollection.find().count() === 0) {
+
+    var houses = [{
+      name: 'Stephan',
+      plants: [{
+        color: 'red',
+        instructions: '3 pots/week'
+      }, {
+        color: 'white',
+        instructions: 'keep humid'
+      }]
+    }]
+
+  }
+
+  while (houses.length > 0) {
+    HousesCollection.insert(houses.pop())
+  }
+
+  console.log('Added fixtures')
+})
